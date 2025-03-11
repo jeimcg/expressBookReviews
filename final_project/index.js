6da7e286132916b34aee2bb1c96d4eca90c4630d
jeimcg <mcgilljei@gmail.com>
2025-03-11 17:03:57 -0400

    update + test general user routes

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
+const { users } = require('./router/auth_users.js');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -11,8 +12,54 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+// register a new user
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // check if both user and pass are provided
+    if (username && password) {
+        // check if the user does not already exist
+        if (!doesExist(username)) {
+            // add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(400).json({message: "User already exists!"});
+        }
+    } 
+    // return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
 });
+
+// Check if a user with the given username already exists
+const doesExist = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Check if the user with the given username and password exists
+const authenticatedUser = (username, password) => {
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+app.use(session({secret:"fingerpint"},resave=true,saveUninitialized=true));
  
 const PORT =5000;
 
